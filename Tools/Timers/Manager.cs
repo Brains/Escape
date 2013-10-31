1507b36ee01725f66971d20d02849a3f6f630054
Brains <completebrains@gmail.com>
2013-10-31 12:25:03 +0200

    Timers

---
+++ Tools/Timers/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Tools.Timers
+{
+    public class Manager : GameComponent
+    {
+        private List <Timer> Timers = new List <Timer> ();
+        private List <Timer> ToRemove = new List <Timer> ();
+
+        public static Manager Instance;
+
+        //------------------------------------------------------------------
+        public Manager (Game game) : base (game)
+        {
+            Instance = this;
+        }
+
+        //------------------------------------------------------------------
+        public static void Add (Timer Timer)
+        {
+            Instance.Timers.Add (Timer);
+        }
+
+        //------------------------------------------------------------------
+        public static void Remove (Timer Timer)
+        {
+            Instance.ToRemove.Add (Timer);
+        }
+
+        //------------------------------------------------------------------
+        public override void Update (GameTime gametime)
+        {
+            foreach (var timer in Timers) 
+                timer.Update ((float) gametime.ElapsedGameTime.TotalSeconds);
+
+            foreach (var timer in ToRemove)
+                Timers.Remove (timer);
+
+            ToRemove.Clear ();
+        }
+    }
+}
---
+++ Tools/Timers/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tools.Timers
+{
+    public class Timer
+    {
+        public Action Trigger;
+        public float Interval;
+        private float elapsed;
+
+        //------------------------------------------------------------------
+        public Timer () {}
+
+        //------------------------------------------------------------------
+        public void Update (float seconds)
+        {
+            elapsed += seconds;
+
+            if (elapsed >= Interval)
+            {
+                Trigger.Invoke ();
+                Destroy ();
+            }
+        }
+
+        //------------------------------------------------------------------
+        public void Destroy ()
+        {
+            Manager.Remove (this);
+        }
+
+        //------------------------------------------------------------------
+        public static void Create (float interval, Action trigger)
+        {
+            Timer timer = new Timer () {Interval = interval, Trigger = trigger};
+
+            Manager.Add (timer);
+        }
+    }
+}
