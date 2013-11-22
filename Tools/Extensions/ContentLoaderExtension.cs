9d2ac6ef6501a79f2c46ed93940e2e9b51acea57
Brains <completebrains@gmail.com>
2013-11-22 10:15:34 +0200

    Add to Previous Commit

--- Tools/Extensions/ContentLoaderExtension.cs
+++ Tools/Extensions/ContentLoaderExtension.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Android.Content;
 using Microsoft.Xna.Framework.Content;
 
 namespace Tools.Extensions
@@ -9,25 +11,55 @@ namespace Tools.Extensions
     {
         public static Dictionary<string, T> LoadContentFolder<T> (this ContentManager contentManager, String contentFolder)
         {
-            // Load directory info, abort if none
-            DirectoryInfo dir = new DirectoryInfo (contentManager.RootDirectory + "\\" + contentFolder);
-            if (!dir.Exists)
-                throw new DirectoryNotFoundException ();
-
             // Init the resulting list
             Dictionary<String, T> result = new Dictionary<String, T> ();
+            LoadManual (contentManager, contentFolder, result);
 
-            // Load all files that matches the file filter
-            FileInfo[] files = dir.GetFiles ("*.*");
-            foreach (FileInfo file in files)
-            {
-                string key = Path.GetFileNameWithoutExtension (file.Name);
-
-                result[key] = contentManager.Load<T> (contentFolder + "/" + key);
-            }
-
-            // Return the result
             return result;
+
+//            // Load directory info, abort if none
+//            DirectoryInfo dir = new DirectoryInfo (contentManager.RootDirectory + "/" + contentFolder);
+//            if (!dir.Exists)
+//                throw new DirectoryNotFoundException ();
+//
+//            // Load all files that matches the file filter
+//            FileInfo[] files = dir.GetFiles ("*.*");
+//            foreach (FileInfo file in files)
+//            {
+//                string key = Path.GetFileNameWithoutExtension (file.Name);
+//
+//                result[key] = contentManager.Load<T> (contentFolder + "/" + key);
+//            }
+//
+//            // Return the result
+//            return result;
+        }
+
+        //------------------------------------------------------------------
+        private static void LoadManual <T> (ContentManager contentManager, string contentFolder, Dictionary <string, T> result)
+        {
+            string key = "";
+
+            key = "Acceleration";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Blinker";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Brake";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Car (Heavy)";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Car (Light)";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Car (Medium)";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Explosion";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Player";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Police";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
+            key = "Road";
+            result[key] = contentManager.Load <T> (contentFolder + "/" + key);
         }
     }
 }
