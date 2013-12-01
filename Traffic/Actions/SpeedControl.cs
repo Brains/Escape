a49eee7c55e699ea7e195d6377adc2f9adbee583
Brains <completebrains@gmail.com>
2013-12-01 12:48:01 +0200

    Speed Control action

---
+++ Traffic/Actions/SpeedControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Traffic.Actions.Base;
+using Traffic.Cars;
+using Traffic.Drivers;
+
+namespace Traffic.Actions
+{
+    public class SpeedControl : Base.Sequence
+    {
+        Driver driver;
+        
+        //------------------------------------------------------------------
+        public SpeedControl(Driver driver)
+        {
+            this.driver = driver;
+            Name = "Speed Control";
+
+            Add (new Generic (Start));
+        }
+
+        //------------------------------------------------------------------
+        public override Base.Action Copy()
+        {
+            return new SpeedControl (driver);
+        }
+
+        //------------------------------------------------------------------
+        public void Start ()
+        {
+            Car closest = driver.FindClosestCar (driver.Car.Lane.Cars.Where (driver.IsAhead));
+
+            if (driver.Distance (closest) > driver.GetSafeZone ())
+                driver.Accelerate (this);
+        }
+    }
+}
